24b9efef5c3d0ad6df09c5f80144c28131d4ae60
Sofia Alvarez Lopez <ms.alvarezl@uniandes.edu.co>
2022-11-15 09:03:01 -0500

    Corregido warning en el hook

--- src/components/detail.js
+++ src/components/detail.js
@@ -12,7 +12,7 @@ export default function Detail() {
      .then((data) => {
        setMascota(data.find(mascota => mascota.id === parseInt(params.mascotaId)));
      });
- }, []);
+ }, [params.mascotaId]);
  return (
    <div>
      <h1>{mascotaPedida.nombre}</h1>
